08818f32ac274691d5a810120de240db4c9b979c
Adams Adeyiga <adamsadeyiga1@gmail.com>
2023-10-18 15:23:33 +0100

    Bug fix

--- monty.c
+++ monty.c
@@ -73,7 +73,7 @@ void push(stack_t **stack, int value)
 	stack_t *new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
+		fprintf(stdout, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
